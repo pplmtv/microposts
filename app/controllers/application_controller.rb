4ebdd735846e9ca34feb35fe2e18adfc2f5fc58a
“yuu.yamazaki” <pplmtv@gmail.com>
2020-10-01 14:14:48 +0000

    follow, unfollow

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,11 +6,13 @@ class ApplicationController < ActionController::Base
     
     def require_user_logged_in
         unless logged_in?
-            require_to login_url
+            redirect_to login_url
         end
     end
     
     def counts(user)
         @count_microposts = user.microposts.count
+        @count_followings = user.followings.count
+        @count_followers = user.followers.count
     end
 end
